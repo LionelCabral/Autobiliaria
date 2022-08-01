48b612522ac4e342faffc0216cd966748dbec91c
lionel <cabrallionel@gmail.com>
2022-08-01 15:49:16 -0300

    entrega del cart view

--- src/component/cart/Cart.jsx
+++ src/component/cart/Cart.jsx
@@ -1,7 +1,41 @@
-import React from 'react';
+import {useContext} from 'react'
+import {CartContext} from '../../context/cartContext'
+import {Link} from 'react-router-dom'
+import './Cart.css'
 
 const Cart = () => {
-    return <div>Cart</div>;
-};
+    const {removeItem, clear, total, cart} = useContext(CartContext);
 
+    if (cart.length === 0) {
+        return (
+            <h2>
+                aun no hay productos volver al <Link to="/">Home</Link>
+            </h2>
+        )
+    }
+    return(
+        <div>
+            {cart.map((prod) => (
+                <div 
+                    key={prod.id}
+                    style={{
+                        border: '1px solid black',
+                        margin: '10px',
+                        display: 'flex',
+                    }}
+                >
+                <img src={prod.img} alt="producto" className="imgCarrito" width="70px" />
+                <div>
+                    <h2>{prod.name}</h2>
+                    <h2>$ {prod.price}</h2>
+                </div>
+                <button onClick={() => removeItem(prod.id)}>Eliminar</button>
+                </div>
+            )
+            )}
+        <button onClick={clear}>Eliminar todos los productos</button>
+        <h3>Total: $ {total()}</h3>
+        </div>
+    )
+}
 export default Cart;
--- src/context/cartContext.jsx
+++ src/context/cartContext.jsx
@@ -15,7 +15,7 @@ const Provider = (props) => {
     //Agrego los Items a mi estado del context y los sumo al carrito.
     const addItem = (item, cantidad) => {
         if (isInCart(item.id)) {
-            alert('Ya está en el carrito');
+            sumarCantidad(cantidad, item);
         } else {
             setCart([...cart, { ...item, cantidad }]);
             console.log(cart)
@@ -32,13 +32,37 @@ const Provider = (props) => {
         return cart.some((item) => item.id === id);
     };
 
+    //Funcion para mostrar las unidades al lado del carrito
+    const unidades = () => {
+    const numeros = cart.reduce((x, y) => x + y.cantidad, 0);
+    return numeros;
+    };
+
+    //Funcion para sumar el total
+    const total = () => {
+    const sumaTotal = cart.reduce((x, y) => x + y.price * y.cantidad, 0);
+    return sumaTotal;
+    };
+
+    //funcion para sumar la cantida de items del mismo producto
+    const sumarCantidad = (counter, item) => {
+    const cantidad = [...cart];
+    cantidad.forEach((c) => {
+    c.id === item.id && (c.cantidad += counter);
+    });
+
+    setCart(cantidad);
+    };
+
     return (
         <CartContext.Provider 
             value={{ 
                 cart, 
                 addItem,
                 removeItem,
-                clear
+                clear,
+                unidades,
+                total
             }}>
             {props.children}
         </CartContext.Provider>
